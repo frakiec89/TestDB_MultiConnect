cc55b959a01b97b93556445974e094fa53058a42
Ahtyamov <Ahtyamov@WIN-SFUQ8BAFB06>
2023-06-28 17:32:45 +0400

    Добавьте файлы проекта.

---
+++ TestDB_MultiConnect/MyDbTestConnect.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestDB_MultiConnect
+{
+    public class MyDbTestConnect
+    {
+
+        private List<string> _hosts; // ip
+        private string cs = string.Empty; // строка  соединения 
+
+        public MyDbTestConnect(string nameDb,  string login , string password, List<string> hosts)
+        {
+             _hosts = hosts;
+            cs = $"Database={nameDb}; User Id= {login}; password={password};TrustServerCertificate=true; Connect Timeout=2;";
+
+
+
+        }
+
+        /// <summary>
+        /// соединяет строку  адрес  сервера  и  остальное 
+        /// </summary>
+        /// <param name="host"></param>
+        /// <returns></returns>
+        private string GetConnectString (string host)
+        {
+            return $"server={host};{cs}";
+        }
+
+        /// <summary>
+        /// Возвращает  строку соединения
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task<string> GetIsTryAsync ()
+        {
+            for (int i = 0; i < _hosts.Count; i++)
+            {
+                string h = _hosts[i];
+                string connect = GetConnectString (h);
+                bool status = await CheckDBAsync(connect); 
+                if (status) 
+                    return connect; // если успешно
+            }
+            throw new Exception("Сервер не доступен по любым адресам");
+        }
+
+
+        /// <summary>
+        /// проверяет  сервер 
+        /// </summary>
+        /// <param name="connectString"></param>
+        /// <returns></returns>
+        private  async Task<bool> CheckDBAsync(string connectString)
+        {
+            using (var connection = new SqlConnection(connectString))
+            try
+            {
+                connection.OpenAsync().Wait(500);
+                if(connection.ServerVersion != null)
+                return true;
+                else
+                return false;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ TestDB_MultiConnect_console/Program.cs
@@ -0,0 +1,37 @@
+﻿using TestDB_MultiConnect;
+
+
+string database = "master";
+string login = "stud";
+string password = "stud";
+
+List<string> hosts = GetIP();
+
+
+
+MyDbTestConnect testConnect = new MyDbTestConnect(database , login , password , hosts);
+try
+{
+    string cs = testConnect.GetIsTryAsync().Result;
+    Console.WriteLine($"Активная строка; {cs}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+List<string> GetIP() // cлучайные  апи
+{
+    List<string> hosts = new List<string>();
+
+    for (int i = 0; i < 10; i++)
+    {
+        Random random = new Random();
+        hosts.Add($"192.168.49.{random.Next(1, 256)}");
+    }
+
+    hosts.Add("192.168.49.180"); // верный ответ
+       return hosts;
+}
